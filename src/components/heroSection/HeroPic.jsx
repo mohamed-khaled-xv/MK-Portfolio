17ab7d3241c647a198d24734cfc4105721ffa113
mohamed-khaled-xv <mohamedkhaledxv8@gmail.com>
2025-07-31 19:11:02 +0300

    minor bug fix

--- src/components/heroSection/HeroPic.jsx
+++ src/components/heroSection/HeroPic.jsx
@@ -23,22 +23,6 @@ const HeroPic = () => {
           />
         </div>
       </div>
-
-      {/* Simple visible test elements - no blur, solid colors */}
-      <div className="absolute inset-0 z-0 pointer-events-none">
-        {/* Test visible circles */}
-        <div className="absolute top-[20%] left-[20%] w-16 h-16 bg-cyan rounded-full animate-pulse"></div>
-        <div className="absolute top-[60%] right-[20%] w-20 h-20 bg-orange rounded-full animate-bounce"></div>
-        <div className="absolute bottom-[30%] left-[30%] w-12 h-12 bg-blue-500 rounded-full animate-ping"></div>
-        
-        {/* Test geometric shapes */}
-        <div className="absolute top-[10%] right-[30%] w-8 h-8 bg-cyan rotate-45 animate-spin"></div>
-        <div className="absolute bottom-[40%] right-[10%] w-6 h-6 bg-orange animate-pulse"></div>
-        
-        {/* Test lines */}
-        <div className="absolute top-[30%] left-[10%] w-1 h-20 bg-cyan animate-pulse"></div>
-        <div className="absolute bottom-[50%] right-[40%] w-20 h-1 bg-orange animate-pulse"></div>
-      </div>
     </motion.div>
   );
 };
